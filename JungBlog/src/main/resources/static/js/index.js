dbd4f6b9242b1fa5fe0d00fb3ba7a133455bdf26
Joo HyunWoo <joois123@naver.com>
2024-03-13 17:58:59 +0900

    3/13

--- JungBlog/src/main/resources/static/js/index.js
+++ JungBlog/src/main/resources/static/js/index.js
@@ -13,6 +13,15 @@ $(function() {
 function view(idx) {
 	location.href = "/blog/" + idx
 }
+function viewblog(idx) {
+	location.href = "/blog/" + idx
+}
+function viewgallery(idx) {
+	location.href = "/blog/" + idx
+}
+function viewfile(idx) {
+   location.href = "/fileboard/blog/" + idx;
+}
 
 //back 눌렀을때 리로드하는 함수
 window.onpageshow = function(event) {
