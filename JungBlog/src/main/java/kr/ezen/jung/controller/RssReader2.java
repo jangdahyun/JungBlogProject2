5db9d6625bc69be767ed987948123b87ec32af90
kimc <tjdtlr12349@gmail.com>
2024-03-28 11:12:15 +0900

    qwfqwfqwfqwf

--- JungBlog/src/main/java/kr/ezen/jung/controller/RssReader2.java
+++ JungBlog/src/main/java/kr/ezen/jung/controller/RssReader2.java
@@ -28,26 +28,33 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 @EnableScheduling
-public class RssReader {
-	private static final String RSS_URL = "https://www.hankyung.com/feed/all-news";
+public class RssReader2 {
+	private static final String RSS_URL = "https://www.hankyung.com/all-news";
 	private List<Item> previousItem = new ArrayList<>();
 	
 	@Autowired
 	private RssDAO rssDAO;
 	
-
-	// @Scheduled(fixedRate = 60000) // 1분마다 실행
+	@Scheduled(fixedRate = 5 * 60000) // 1분마다 실행
     public void checkForUpdates() {
 		log.info("뉴스 읽기 시작");
         try {
         	List<Item> currentItems = readRss(); // 현재 가져온 뉴스들!
             for (Item item : currentItems) {
                 if (!previousItem.contains(item)) {
-                	if(item.getAuthor() == null || item.getAuthor().length()==0) {
+                	
+                	Map<String, String> data = getData(item.getLink());
+                	item.setCategory(data.get("catogory"));
+                	item.setPubDate(data.get("pubDate"));
+                	item.setAuthor(data.get("author"));
+                	if(item.getAuthor() == null || item.getAuthor().length() == 0) {
                 		item.setAuthor(" ");
                 	}
+                	
+                	log.info("item => {}", item);
+                	/*
                 	SimpleDateFormat originalFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
-                	Date date;
+                	Date date = null;
                 	try {
                 	    date = originalFormat.parse(item.getPubDate());
                 	} catch (ParseException e) {
@@ -56,15 +63,11 @@ public class RssReader {
                 	}
                 	SimpleDateFormat newFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 	String formattedDate = newFormat.format(date);
-
+				
                 	// 포맷팅된 문자열을 item 객체에 설정
                 	item.setPubDate(formattedDate);
-                	Map<String, String> data = getData(item.getLink());
-                	item.setCategory(data.get("catogory"));
-                	item.setImage(data.get("str"));
-                	if(item.getImage() == null || item.getImage().equals("")) {
-                		item.setImage(" ");
-                	}
+                	*/
+                	
                 	int check = rssDAO.findByLink(item.getLink());
                 	if(check != 1) {
                 		rssDAO.insert(item);
@@ -82,14 +85,15 @@ public class RssReader {
     private static List<Item> readRss() throws IOException {
         List<Item> items = new ArrayList<>();
         Document doc = Jsoup.connect(RSS_URL).get();
-        Elements rssItems = doc.select("item"); // RSS 피드의 항목 선택
-        for (Element rssItem : rssItems) {
+        Elements rssLis = doc.select(".news-list > li"); // RSS 피드의 항목 선택
+        for (Element rssLi : rssLis) {
             Item item = new Item();
-            item.setTitle(rssItem.select("title").text());
-            item.setLink(rssItem.select("link").text());
-            item.setImage(rssItem.select("image").text());
-            item.setAuthor(rssItem.select("author").text());
-            item.setPubDate(rssItem.select("pubDate").text());
+            item.setTitle(rssLi.select(".news-tit a").text());
+            
+            item.setLink(rssLi.select(".news-tit a").attr("href"));
+            
+            item.setImage(rssLi.select(".thumb img").attr("src"));
+            
             items.add(item);
         }
         Collections.reverse(items);
@@ -99,19 +103,37 @@ public class RssReader {
     private static Map<String, String> getData(String urlAddress) throws IOException{
     	Map<String, String> map = new HashMap<>();
     	Document doc = Jsoup.connect(urlAddress).get();
-    	Elements breadcrumb = doc.select(".breadcrumb");
-    	Elements figureImg = doc.select(".figure-img");
+    	Elements breadcrumb = doc.select(".breadcrumb");		// 카테고리
+    	Elements pubDateEl = doc.select(".txt-date");			// 게시일
+    	Elements authorEl = doc.select(".author .item");		// 저자
 		if(breadcrumb != null && breadcrumb.size() > 0) {
             Element category = breadcrumb.first().selectFirst("a");
             if(category != null) {
                  map.put("catogory", category.text());
             }
         }
+		/*
 		if(figureImg != null && figureImg.size() > 0) {
 			Elements imgTags = figureImg.select("img");
 			String src = imgTags.get(0).attr("src");
 			if (src != null && !src.equals("")) {
 				map.put("str", src);
+			}
+		}
+		*/
+		
+		if(pubDateEl != null && pubDateEl.size() > 0) {
+			Element pubDate = pubDateEl.first();
+			if(pubDate != null) {
+				//log.info("pubDate => {}", pubDate.text());
+                map.put("pubDate", pubDate.text());
+           }
+		}
+		if(authorEl != null && authorEl.size() > 0) {
+			Element author = authorEl.first();
+			if(author != null) {
+				//log.info("pubDate => {}", pubDate.text());
+				map.put("author", author.text());
 			}
 		}
 		return map;
